76004b014c37dc5530384fed32fd4d4b3414d3ee
Seokho Youn <axlover@hotmail.com>
2018-12-10 17:31:34 +0900

    save

--- src/com/mvc/model/Calculate.java
+++ src/com/mvc/model/Calculate.java
@@ -1,5 +1,8 @@
 package com.mvc.model;
 
 public interface Calculate {
-	public void calc(); // 급여 계산용
+	// 상수필드와 추상메소드로만 멤버 구성함
+	//public static final 자료형 필드명 = 초기값;
+	
+	/*public abstract*/ void calc(); // 급여 계산용
 }
